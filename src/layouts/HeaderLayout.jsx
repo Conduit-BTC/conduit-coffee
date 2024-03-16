3ef98023cd6b24e2c05dbd96aba4bdb696ecabe4
aceaspades <aceaspades@protonmail.com>
2024-03-16 03:44:30 -0700

    Ride the Lightning

--- src/layouts/HeaderLayout.jsx
+++ src/layouts/HeaderLayout.jsx
@@ -2,7 +2,7 @@ const HeaderLayout = () => {
   return (
     <header className="bg-[var(--cart-secondary-bg-color)] p-2">
       <h3 className="my-4 text-left lg:text-center text-[var(--main-text-color)]">
-        {`⚡️ `}Welcome to Conduit{` ⚡️`}
+        {`⚡️ `}Ride The Lightning{` ⚡️`}
       </h3>
       {/* <h3 className="p-4 pl-2 text-md lg:text-xl">
         $ conduit_coffee {`->`}{" "}
