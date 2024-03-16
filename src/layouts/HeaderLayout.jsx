9ed735a51b8e0739190dd19e43c56cec69376f74
aceaspades <aceaspades@protonmail.com>
2024-03-16 03:20:11 -0700

    Reduced header size

--- src/layouts/HeaderLayout.jsx
+++ src/layouts/HeaderLayout.jsx
@@ -5,10 +5,10 @@ const HeaderLayout = () => {
         ⚡ Conduit Coffee {`->`}{" "}
         <span className="font-normal">for/the/people</span>
       </h2> */}
-      <h2 className="p-4 pl-2">
+      <h3 className="p-4 pl-2">
         $ conduit_coffee {`->`}{" "}
         <span className="font-normal">coffee/for/the_people</span>
-      </h2>
+      </h3>
     </header>
   );
 };
