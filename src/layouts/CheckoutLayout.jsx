fb189ef02076b8a9b6b0b0bec983e2d6eeb83914
aceaspades <aceaspades@protonmail.com>
2024-03-17 02:36:53 -0700

    All UI ready for Lightning integration

--- src/layouts/CheckoutLayout.jsx
+++ src/layouts/CheckoutLayout.jsx
@@ -10,24 +10,67 @@ export default function CheckoutLayout() {
     <>
       <h1 className="text-h2 pr-12 text-blue-500 mb-2">⚡️ Zap Lightning</h1>
       <h1 className="text-h2 pr-12 text-orange-700 mb-12">☕️ Get Coffee</h1>
-      <h3 className="mb-4">{`> Shipping Address`}</h3>
+      <h3 className="mb-2">{`Shipping Address`}</h3>
       <h6>{`We don't need to know you, we just need a place to send your coffee`}</h6>
-      <input
-        className="w-full p-2 mt-4"
-        type="text"
-        placeholder="Street Address"
-      />
-      <input
-        className="w-full p-2 mt-4"
-        type="text"
-        placeholder="Street Address (line 2)"
-      />
-      <input className="w-full p-2 mt-4" type="text" placeholder="City" />
-      <input className="w-full p-2 mt-4" type="text" placeholder="State" />
-      <input className="w-full p-2 mt-4" type="text" placeholder="Zip Code" />
-      <button className="w-full p-2 mt-4 bg-blue-500 text-[var(--main-text-color)] hover:font-bold">
-        {`>> Pay With Lightning <<`}
-      </button>
+      <form>
+        <input
+          className="w-full p-2 mt-4"
+          type="address"
+          placeholder="Street Address"
+          id="address-1"
+          required
+        />
+        <input
+          className="w-full p-2 mt-4"
+          type="address"
+          placeholder="Street Address (line 2)"
+          id="address-2"
+        />
+        <input
+          className="w-full p-2 mt-4"
+          type="text"
+          placeholder="City"
+          id="city"
+        />
+        <input
+          className="w-full p-2 mt-4"
+          type="text"
+          placeholder="State"
+          id="state"
+        />
+        <input
+          className="w-full p-2 mt-4"
+          type="text"
+          placeholder="Zip Code"
+          id="zip"
+        />
+        <input
+          className="w-full p-2 mt-4"
+          type="text"
+          placeholder="Special Instructions?"
+          id="special-instructions"
+        />
+        <h3 className="mt-8 mb-2">
+          {`Contact Info`}
+          <span className="text-sm">{` (Optional)`}</span>
+        </h3>
+        <h6>{`We'll send you a tracking number and receipt, nothing else. Skip it if you're off-the-radar `}</h6>
+        <input
+          className="w-full p-2 mt-4"
+          type="email"
+          placeholder="Email"
+          id="email"
+        />
+        <input
+          className="w-full p-2 mt-4"
+          type="email"
+          placeholder="Nostr handle"
+          id="email"
+        />
+        <button className="w-full p-2 mt-4 bg-blue-500 text-[var(--main-text-color)] hover:font-bold">
+          {`>> Pay With Lightning <<`}
+        </button>
+      </form>
     </>
   );
 }
--- src/layouts/ModalWindowLayout.jsx
+++ src/layouts/ModalWindowLayout.jsx
@@ -6,10 +6,10 @@ export default function ModalWindowLayout({ children }) {
   return (
     <section
       className={`${
-        isModalOpen ? "fixed" : "hidden"
+        isModalOpen ? "absolute" : "hidden"
       } top-0 left-0 w-full h-full p-4 bg-black/75 z-[9999]`}
     >
-      <div className="w-full h-[97.5vh] border-2 border-[var(--main-text-color)] bg-[var(--main-bg-color)] p-4">
+      <div className="w-full min-h-[97.5vh] border-2 border-[var(--main-text-color)] bg-[var(--main-bg-color)] p-4">
         <button
           onClick={closeModal}
           className="absolute top-8 right-8 p-2 w-12 h-12 centered text-4xl bg-[var(--main-text-color)] text-[var(--main-bg-color)]"
