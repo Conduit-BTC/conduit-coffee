ca2105f69551770e3e7e5809f13b02f3fe380517
aceaspades <aceaspades@protonmail.com>
2024-04-04 14:16:54 -0700

    Env var loading

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,13 +2,12 @@ import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
-export default defineConfig((mode) => {
+export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, "./", "");
-
   return {
     plugins: [react()],
     define: {
-      "import.meta.env": env,
+      "import.meta.env": JSON.stringify(env),
     },
   };
 });
