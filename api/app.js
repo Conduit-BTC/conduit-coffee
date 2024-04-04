2e5edf464f74139f3db0a63d79adaebcdd6f5977
aceaspades <aceaspades@protonmail.com>
2024-04-04 00:27:18 -0700

    Updated CORS

--- api/app.js
+++ api/app.js
@@ -3,9 +3,24 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
+const allowedOrigins = [
+  'http://localhost:5173',
+  'conduit-coffee-terminal.vercel.app',
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+  }),
+);
 
 app.use(bodyParser.json());
-app.use(cors());
 
 // Import and use route handlers
 const orderRoutes = require('./routes/orderRoutes');
