9b8b90b02202df6a2beaee3eebf356500ba4ce91
aceaspades <aceaspades@protonmail.com>
2024-04-04 19:51:21 -0700

    Expanded InvoiceSettled

--- api/controllers/invoiceController.js
+++ api/controllers/invoiceController.js
@@ -2,13 +2,18 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
 exports.settleInvoice = async (req, _) => {
-    switch (req.body.type) {
-      case 'InvoicePaymentSettled':
-        console.log('Invoice settled: ', req.body.invoiceId);
-      default:
-        console.log(
-          `Invoice request received: ${req.body.type} - Invoice ID ${req.body.invoiceId}`,
-        );
-    }
-    console.log(req.body);
+  switch (req.body.type) {
+    case 'InvoiceSettled':
+      updateOrderPaymentStatus(req.body);
+    default:
+      console.log(
+        `Invoice request received: ${req.body.type} - Invoice ID ${req.body.invoiceId}`,
+      );
+  }
+  console.log(req.body);
 };
+
+function updateOrderPaymentStatus(data) {
+  const { invoiceId, timestamp } = data;
+  console.log(`Invoice settled: ${invoiceId} - Time: ${timestamp}`);
+}
