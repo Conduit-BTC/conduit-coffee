efd9ea92dd1c172f7f77dfe522dbe27d5380e3db
aceaspades <aceaspades@protonmail.com>
2024-04-26 19:34:40 -0700

    Improvements to ShippingCostCalculator component

--- frontend/src/components/ShippingCostCalculator.jsx
+++ frontend/src/components/ShippingCostCalculator.jsx
@@ -1,35 +1,71 @@
 import { useState } from "react";
 import { useCartContext } from "../context/CartContext";
+import { useCryptoContext } from "../context/CryptoContext";
+import SatsIcon from "./SatsIcon";
 
-export default function ShippingCostCalculator({ zip }) {
+const performCalculation = async (zip) => {
+  const url = "http://localhost:3456/shipping/rate";
+  //   import.meta.env.VITE_API_URL || "https://conduit-service.fly.dev";
+
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ zip, cart: cartItems }),
+  });
+
+  if (response.ok) {
+    const data = await response.json();
+    return usdCost;
+  } else {
+    console.error("Failed to calculate shipping cost:", response.statusText);
+  }
+};
+
+export default function ShippingCostCalculator() {
   const { cartItems } = useCartContext();
-  const [cost, setCost] = useState(0.0);
-
-  const performCalculation = async () => {
-    const url = "http://localhost:3456/shipping/rate";
-    //   import.meta.env.VITE_API_URL || "https://conduit-service.fly.dev";
-
-    const response = await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ zip, cart: cartItems }),
-    });
-
-    if (response.ok) {
-      const data = await response.json();
-      console.log("Total cost: ", data);
-    } else {
-      console.error("Failed to calculate shipping cost:", response.statusText);
-    }
-  };
+  const { satsToUsd } = useCryptoContext();
+  const [satsCost, setSatsCost] = useState(0);
+  const [usdCost, setUsdCost] = useState(0.0);
+  const [zip, setZip] = useState("");
 
   return (
     <section className="flex flex-col">
       <h3>Shipping Calculator</h3>
-      <button onClick={performCalculation}>Calculate Shipping Cost</button>
-      <h4>{`Shipping Cost: $${cost} / ___ Sats`}</h4>
+      <input
+        className="p-2 bg-blue-300 text-black mt-4 placeholder:text-black"
+        type="text"
+        placeholder="Enter ZIP code"
+        onChange={(e) => {
+          setZip(e.target.value);
+        }}
+      />
+      <button
+        disabled={!zip || zip.length < 5}
+        className={`p-4 flex my-4 ${
+          zip && zip.length >= 5 ? "bg-blue-500" : "bg-blue-500/20"
+        }`}
+        onClick={() => {
+          const res = performCalculation();
+          if (res) {
+            setUsdCost(res);
+            setSatsCost(res * satsToUsd);
+          } else {
+            // TODO: Display error state
+          }
+        }}
+      >
+        {`>> Calculate Shipping Cost << `}
+      </button>
+      <h4>
+        Shipping Cost:
+        <span className="text-orange-500">
+          <SatsIcon color="orange" />
+          {`${Math.floor(satsCost)} Sats`}
+        </span>
+        <span className="text-green-500">{` ($${usdCost})`}</span>
+      </h4>
     </section>
   );
 }
