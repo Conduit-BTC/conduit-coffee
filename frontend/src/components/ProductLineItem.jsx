8f12aff24b48eccd0f24f84fde09b282b412a067
aceaspades <aceaspades@protonmail.com>
2024-09-04 22:05:15 -0700

    Style updates

--- frontend/src/components/ProductLineItem.jsx
+++ frontend/src/components/ProductLineItem.jsx
@@ -1,6 +1,7 @@
 import pingBorder from "../lib/pingBorder";
 import { useUiContext } from "../context/UiContext";
 import { useCartContext } from "../context/CartContext";
+import { useState, useEffect } from "react";
 
 export default function ProductLineItem({
   // borderElement,
@@ -14,48 +15,52 @@ export default function ProductLineItem({
 }) {
   const { openProductDetailsModal } = useUiContext();
   const { addItemToCart, removeItemFromCart } = useCartContext();
+  const [ formattedDescription, setFormattedDescription ] = useState('');
   const { name, description } = product;
 
+  useEffect(() => {
+    const d = [...description.split('//').join('\n//')].slice(1);
+    setFormattedDescription(d);
+  }, [description, setFormattedDescription]);
+
   return (
-    <section className="flex">
+    <section className="flex w-full group">
       <button
         aria-label={`Display product information about ${name}`}
         onClick={() => {
           openProductDetailsModal(product);
         }}
-        className="flex items-center p-4 border-2 border-blue-500/50 hover:border-blue-500 "
+        className="flex flex-col p-4 border-2 group-hover:border-blue-500 w-full"
       >
-        <p
-          className={`border-2 rounded-sm p-1 px-3 mr-2 flex items-center justify-center font-[700] ${borderColor}`}
+        <h4 className={`font-[700] ${accentColor}`}>{name}</h4>
+        <span className="whitespace-pre-line text-left mt-4">{formattedDescription}</span>
+      </button>
+      <div className="flex flex-col justify-center group-hover:bg-blue-500 p-2 gap-2 items-center bg-[var(--main-text-color)]">
+      <p
+          className={`border-2 rounded-sm p-1 px-3 flex items-center justify-center bg-black font-[700] ${borderColor}`}
           style={{ fontFamily: "Fira Code" }}
         >
           {quantity || 0}
         </p>
-        <h4 className="font-[700] px-2">
-          <span className={accentColor}>{name}</span>
-          <span className="text-h7">{` (${description})`}</span>
-        </h4>
-      </button>
-      <div className="flex px-2 gap-2 items-center border-2 border-blue-500/75">
-        <button
-          onClick={() => {
-            removeItemFromCart(product);
-          }}
-          aria-label={`Remove ${name} from cart`}
-          className={
-            "p-1 w-8 h-8 lg:w-6 lg:h-6 flex items-center justify-center rounded-sm bg-red-800" +
-            (quantity === 0 ? " opacity-50" : "")
-          }
-          disabled={quantity === 0}
-        >{`-`}</button>
         <button
           onClick={() => {
             // pingBorder(borderElement.current, pingColor);
             addItemToCart(product);
           }}
           aria-label={`Add ${name} to cart`}
-          className="p-1 w-8 h-8 lg:w-6 lg:h-6 flex items-center justify-center rounded-sm bg-green-800"
+          className="p-1 w-8 h-8 lg:w-6 lg:h-6 flex items-center justify-center rounded-sm bg-green-800 shadow-md shadow-black/50"
         >{`+`}</button>
+          <button
+            onClick={() => {
+              removeItemFromCart(product);
+            }}
+            aria-label={`Remove ${name} from cart`}
+            className={
+              "p-1 w-8 h-8 lg:w-6 lg:h-6 flex items-center justify-center rounded-sm bg-red-800 shadow-md shadow-black/50" +
+              (quantity === 0 ? " opacity-50" : "")
+            }
+            disabled={quantity === 0}
+          >{`-`}</button>
       </div>
     </section>
   );
--- frontend/src/layouts/Modals/ProductDetailsLayout.jsx
+++ frontend/src/layouts/Modals/ProductDetailsLayout.jsx
@@ -3,78 +3,99 @@ import { useCartContext } from "../../context/CartContext";
 import { useCryptoContext } from "../../context/CryptoContext";
 import CheckoutButton from "../../components/Buttons/CheckoutButton";
 import SatsIcon from "../../components/SatsIcon";
+import { useState, useEffect } from "react";
 
 export default function ProductDetailsLayout() {
-  const { currentProductDetails, closeProductDetailsModal } = useUiContext();
-  const { cartItems, addItemToCart, removeItemFromCart } = useCartContext();
+  const { currentProductDetails: product } = useUiContext();
+  const [formattedDescription, setFormattedDescription] = useState("");
   const { satsToUsd } = useCryptoContext();
 
-  const { name, description, price } = currentProductDetails;
+  const { name, description, price } = product;
+
+  useEffect(() => {
+    if (setFormattedDescription === undefined) return;
+    const d = [...description.split("//").join("\n//")].slice(1);
+    setFormattedDescription(d);
+  }, [description, setFormattedDescription]);
 
   return (
-    <section className="flex flex-col justify-between gap-24">
-      <div className="mb-4">
+    <section className="flex flex-col">
+      <div className="mb-16">
         <img
           src="/images/conduit-coffee-banner.jpg"
           alt="Conduit Coffee Banner"
         />
       </div>
-      <div className="flex flex-col gap-2">
+      <div className="flex flex-col gap-2 h-full mb-4">
         <h1>{name}</h1>
-        <p>{description}</p>
-        <div className="flex gap-4">
-          <h3 className="text-green-500">
-            {`$${price}`}
-            <span className="text-sm">{`(usd)`}</span>
-          </h3>
-          <h3 className="text-orange-500">
-            <SatsIcon color="orange" />
-            {satsToUsd >= 0.0 ? satsToUsd.toFixed(0) : "-"}
-            <span className="text-sm">{`(sats)`}</span>
-          </h3>
-        </div>
-      </div>
-      <div className="flex gap-2">
-        <p
-          className={`border-2 rounded-sm p-4 mr-2 flex items-center justify-center font-[700] border-blue-500`}
-          style={{ fontFamily: "Fira Code" }}
-        >
-          {cartItems.reduce(
-            (sum, item) =>
-              sum + (item.id === currentProductDetails.id ? item.quantity : 0),
-            0
-          )}
+        <p className="whitespace-pre-line text-left mt-4">
+          {formattedDescription}
         </p>
-
-        <button
-          className="bg-green-500 p-4 font-bold"
-          onClick={() => {
-            addItemToCart(currentProductDetails);
-          }}
-        >
-          Add to Cart
-        </button>
-        <button
-          className="bg-red-500 p-4 font-bold"
-          onClick={() => {
-            removeItemFromCart(currentProductDetails);
-          }}
-        >
-          Remove from Cart
-        </button>
-        <button
-          onClick={() => closeProductDetailsModal()}
-          className="bg-yellow-500 p-4 font-bold ml-auto"
-        >
-          Go Back
-        </button>
-        <div>
-          <CheckoutButton
-            onHandleClick={closeProductDetailsModal}
-            text={`zap out -> buy now`}
-          />
+        <div className="w-full">
+          <div className="flex gap-4 mt-16 mb-4">
+            <h3 className="text-green-500">
+              {`$${price}`}
+              <span className="text-sm">{`(usd)`}</span>
+            </h3>
+            <h3 className="text-orange-500">
+              <SatsIcon color="orange" />
+              {satsToUsd >= 0.0 ? satsToUsd.toFixed(0) : "-"}
+              <span className="text-sm">{`(sats)`}</span>
+            </h3>
+          </div>
+          <div className="">
+            <AddRemoveProduct product={product} />
+          </div>
         </div>
       </div>
     </section>
   );
 }
+
+const AddRemoveProduct = ({ product }) => {
+  const { cartItems, addItemToCart, removeItemFromCart } = useCartContext();
+  const { closeProductDetailsModal } = useUiContext();
+
+  const { name, quantity } = product;
+
+  return (
+    <div className="flex gap-2">
+      <div className="flex items-center gap-2">
+        <p
+          className={`border-2 rounded-sm p-4 mr-2 flex items-center justify-center font-[700] border-blue-500`}
+          style={{ fontFamily: "Fira Code" }}
+        >
+          {cartItems.reduce(
+            (sum, item) => sum + (item.id === product.id ? item.quantity : 0),
+            0
+          )}
+        </p>
+        <button
+          onClick={() => {
+            removeItemFromCart(product);
+          }}
+          aria-label={`Remove ${name} from cart`}
+          className={
+            "p-1 w-12 h-12 flex items-center justify-center rounded-sm bg-red-800 shadow-md shadow-black/50" +
+            (quantity === 0 ? " opacity-50" : "")
+          }
+          disabled={quantity === 0}
+        >{`-`}</button>
+        <button
+          onClick={() => {
+            // pingBorder(borderElement.current, pingColor);
+            addItemToCart(product);
+          }}
+          aria-label={`Add ${name} to cart`}
+          className="p-1 w-12 h-12 flex items-center justify-center rounded-sm bg-green-800 shadow-md shadow-black/50"
+        >{`+`}</button>
+      </div>
+      <div className="ml-2">
+        <CheckoutButton
+          onHandleClick={closeProductDetailsModal}
+          text={`zap out -> buy now`}
+        />
+      </div>
+    </div>
+  );
+};
