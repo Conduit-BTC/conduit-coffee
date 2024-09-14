a0485995a283fb465e9fbbeaad902a07783d4345
aceaspades <aceaspades@protonmail.com>
2024-09-14 11:30:31 -0700

    Removed shipping calculator

--- frontend/src/layouts/Modals/CheckoutLayout.jsx
+++ frontend/src/layouts/Modals/CheckoutLayout.jsx
@@ -48,10 +48,10 @@ export default function CheckoutLayout() {
         <CurrentHodlings />
       </div>
       <div className="w-full h-1 bg-gray-600 mb-8" />
-      <ShippingCostCalculator />
+      {/* <ShippingCostCalculator /> */}
       <div className="w-full h-1 bg-gray-600 my-8" />
       <h3 className="mb-2">{`Shipping Address`}</h3>
-      <h6>{`We don't need to know you, we just need a place to send your coffee. Use a codename if you'd like.`}</h6>
+      <h6>{`We don't need to know you, we just need a place to send your coffee.`}</h6>
       <form
         onSubmit={async (e) => {
           e.preventDefault();
