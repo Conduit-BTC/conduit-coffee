ff4e054b5b61a9b617d8b56167d1a93f3020af16
aceaspades <aceaspades@protonmail.com>
2024-11-29 23:22:48 -0800

    Cart area scrolls with screen sticky

--- frontend/src/layouts/CartLayout.jsx
+++ frontend/src/layouts/CartLayout.jsx
@@ -5,7 +5,7 @@ import CheckoutButton from "../components/Buttons/CheckoutButton";
 
 export default function CartLayout() {
   return (
-    <section className="z-100 w-full lg:w-1/2 lg:display min-h-screen right-0 top-0 p-2 ">
+    <section className="z-50 w-full lg:w-1/2 h-screen right-0 top-0 p-2 sticky">
       <div className="h-full p-2 bg-[var(--cart-bg-color)] text-[var(--cart-text-color)] flex flex-col gap-4">
         {/* Header */}
         <div className="w-full p-2 bg-[var(--cart-secondary-bg-color)]">
