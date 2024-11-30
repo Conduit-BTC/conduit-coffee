156d3c5439c6cac5b14b657da3dae7188c73593c
aceaspades <aceaspades@protonmail.com>
2024-11-29 22:58:02 -0800

    Improved Delivery Times message

--- frontend/src/components/EarlyAdopterMessage.jsx
+++ frontend/src/components/EarlyAdopterMessage.jsx
@@ -12,7 +12,7 @@ export const DeliveryTimesMessage = () => {
         <>
             <h5 className='mb-2'>Early Adopters - Expected Delivery Times</h5>
             <div className='mb-4'>
-                {`We are a small company, just getting started. We have enough stock on-hand to fulfill our expected sales volume. However, if we’re more-popular than expected, your order may be delayed by up to 2 weeks. This is because we must order beans, bags, labels, and packing materials, then ship them out to you.`}
+                {`We are a small company, and we're just getting started. We have enough stock on-hand to fulfill our expected sales volume. However, if we’re more-popular than expected, your order may be delayed by up to 2 weeks. This is because we must order beans, bags, labels, and packing materials, then ship them out to you.`}
             </div>
             <div className='mb-4'>{`Keep a copy of your receipt on-hand, and if you have any questions or concerns, reach out to us via `}<a className='font-bold' href="https://primal.net/p/npub1nkfqwlz7xkhhdaa3ekz88qqqk7a0ks7jpv9zdsv0u206swxjw9rq0g2svu" target="_blank">Nostr DM</a> or <b>email: contact (at) conduitbtc (dot) com</b>.</div>
             <div>{`You’re supporting a brand-new Nostr Lightning project, and we thank you immensely for your patience.`}</div>
